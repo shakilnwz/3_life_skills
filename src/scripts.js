948738dece3521bb15627105a96ea044fcf0ca46
Shakil Nawaz <shakilnwz@gmail.com>
2023-12-19 01:53:52 +0600

    all done except carousel

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,27 @@
+//nav menu
+const threeDot = document.querySelector('.nav__bar')
+const navMenu = document.querySelector('.nav__menu')
+
+threeDot.addEventListener('click', (e) => {
+    console.log(e)
+    if (e.target.dataset.visibility === 'false') {
+        navMenu.style.visibility = 'initial'
+        // navMenu.style.width = '60vw'
+        e.target.dataset.visibility = 'true'
+    } else if (e.target.dataset.visibility === 'true') {
+        navMenu.style.visibility = 'hidden'
+        // navMenu.style.width = '0vw'
+
+
+        e.target.dataset.visibility = 'false'
+    }
+
+})
+// window.addEventListener('resize', () => {
+//     location.reload()
+// })
+
+//expand all
 const expandAll = document.querySelector('#expand_all')
 const accordianAll = document.querySelectorAll('details')
 
